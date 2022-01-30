5a4c96365f67396f1f8245bdef8c5fba51b9874d
Shreya Sandal <sandalshreya@gmail.com>
2022-01-30 12:16:06 +0530

    Commiting all at once

---
+++ src/main/java/com/codewithshreya/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.codewithshreya.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
+		return String.format("Hello %s", name);
+	}
+}
